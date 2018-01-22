57fae48b9c590c4061fd62ab5219752bbc469610
IvannaIvanova <35695048+IvannaIvanova@users.noreply.github.com>
2018-01-22 17:39:15 +0200

    Add files via upload
    Program that tells if the word is a fruit of a vegetable

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zelenchuk_vs_plod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var product = Console.ReadLine();
+            if (product == "banana" || product == "apple" || product == "kiwi" || product == "cherry" || product == "lemon" ||
+                product == "grapes")
+            {
+                Console.WriteLine("fruit");
+            }
+            else if (product == "tomato" || product == "carrot" || product =="cucumber" || product =="pepper" ) 
+            {
+                Console.WriteLine("vegetable");
+            } else
+           
+            {
+                Console.WriteLine("unknown");
+            }
+        }
+    }
+}
